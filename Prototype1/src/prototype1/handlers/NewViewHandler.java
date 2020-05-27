01dffab432e424166c5db8ecb50dacf754f40de1
BluebirdLyrase <pisitkeawfung@gmail.com>
2020-05-27 12:56:32 +0700

    Create abd use new view in Prototype1
    เรียกใช้ View โดยการใช้
    HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView(viewId);
    
    สร้าง View โดยการใช้  wizard ใน

--- Prototype1/src/prototype1/handlers/NewViewHandler.java
+++ Prototype1/src/prototype1/handlers/NewViewHandler.java
@@ -4,6 +4,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 
@@ -12,10 +13,16 @@ public class NewViewHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Prototype1",
-				"Hello, Eclipse world");
+//		MessageDialog.openInformation(
+//				window.getShell(),
+//				"Prototype1",
+//				"Hello, Eclipse world");
+		try {
+			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView("prototype1.NewViews.NewViews");
+		} catch (PartInitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
