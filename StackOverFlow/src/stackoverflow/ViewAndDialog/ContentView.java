9ee7c3d666345a8cef047ce3666ed9447fa5ef62
tewtew023 <abcdtivolitwin@gmail.com>
2020-06-03 15:49:28 +0700

    add backgroud color to label

--- StackOverFlow/src/stackoverflow/ViewAndDialog/ContentView.java
+++ StackOverFlow/src/stackoverflow/ViewAndDialog/ContentView.java
@@ -1,7 +1,6 @@
 package stackoverflow.ViewAndDialog;
 
 
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.*;
 //import org.eclipse.jface.viewers.*;
 //import org.eclipse.swt.graphics.Image;
@@ -13,6 +12,7 @@ import org.eclipse.swt.*;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.*;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Device;
@@ -43,147 +43,39 @@ public class ContentView extends ViewPart {
 	 * The ID of the view as specified by the extension.
 	 */
 	public static final String ID = "stackoverflow.ViewAndDialog.ContentView";
-	private Device device ;
 	 @Override
 	    public void createPartControl(Composite parent) {
 		 
-	        Text viewer = new Text(parent, SWT.BORDER);
-	        viewer.setText("Imagine a fantastic user interface here");
-	        Label label = new Label(parent, SWT.NONE);
-	        Color color = new Color(device, SWT.COLOR_RED,SWT.COLOR_GREEN,SWT.COLOR_BLUE);
-	        label.setText("This is a Gorgeous label");
+
+	        StyledText sText = new StyledText(parent, SWT.CENTER);
+	    	sText.setText("0123456789 ABCDEFGHIJKLM NOPQRSTUVWXYZ");
+	    	// make 0123456789 appear bold
+	    	StyleRange style1 = new StyleRange();
+	    	style1.start = 0;
+	    	style1.length = 10;
+	    	style1.fontStyle = SWT.BOLD;
+	    	sText.setStyleRange(style1);
+
+	        Label label = new Label(parent,SWT.CENTER);
+	        Color col = new Color(null, 150,100,100);
+	        parent.setLayout (new FillLayout ());
 	        
-	        StyledText sText = new StyledText(parent, SWT.NONE);
-	        color.getBlue();
-	        sText.setBackground(color);
-	        sText.setText(label.getText());
-	    }
+	    	new Label (parent, SWT.SEPARATOR | SWT.VERTICAL);
+	    	parent.setSize (200, 200);
+	    	label.setText("LABEL");
+	    	
+	    	label.setBackground(col);
+	    	
+	    	
+	    	}
+	 		
+
+	         
+	    
 	 
 
 	    @Override
 	    public void setFocus() {
 	    }
 	
-//	private TableViewer viewer;
-//	private Action action1;
-//	private Action action2;
-//	private Action doubleClickAction;
-//	 
-//
-//	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
-//		@Override
-//		public String getColumnText(Object obj, int index) {
-//			return getText(obj);
-//		}
-//		@Override
-//		public Image getColumnImage(Object obj, int index) {
-//			return getImage(obj);
-//		}
-//		@Override
-//		public Image getImage(Object obj) {
-//			return workbench.getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
-//		}
-//	}
-//
-//	@Override
-//	public void createPartControl(Composite parent) {
-//		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-//		viewer.setContentProvider(ArrayContentProvider.getInstance());
-//		viewer.setInput(new String[] { "One", "Two", "Three" });
-//	viewer.setLabelProvider(new ViewLabelProvider());
-//
-//
-//		// Create the help context id for the viewer's control
-//		workbench.getHelpSystem().setHelp(viewer.getControl(), "StackOverFlow.viewer");
-//		getSite().setSelectionProvider(viewer);
-//		makeActions();
-//		hookContextMenu();
-//		hookDoubleClickAction();
-//		contributeToActionBars();
-//	}
-//
-//	private void hookContextMenu() {
-//		MenuManager menuMgr = new MenuManager("#PopupMenu");
-//		menuMgr.setRemoveAllWhenShown(true);
-//		menuMgr.addMenuListener(new IMenuListener() {
-//			public void menuAboutToShow(IMenuManager manager) {
-//				ContentView.this.fillContextMenu(manager);
-//			}
-//		});
-//		Menu menu = menuMgr.createContextMenu(viewer.getControl());
-//		viewer.getControl().setMenu(menu);
-//		getSite().registerContextMenu(menuMgr, viewer);
-//	}
-//
-//	private void contributeToActionBars() {
-//		IActionBars bars = getViewSite().getActionBars();
-//		fillLocalPullDown(bars.getMenuManager());
-//		fillLocalToolBar(bars.getToolBarManager());
-//	}
-//
-//	private void fillLocalPullDown(IMenuManager manager) {
-//		manager.add(action1);
-//		manager.add(new Separator());
-//		manager.add(action2);
-//	}
-//
-//	private void fillContextMenu(IMenuManager manager) {
-//		manager.add(action1);
-//		manager.add(action2);
-//		// Other plug-ins can contribute there actions here
-//		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-//	}
-//	
-//	private void fillLocalToolBar(IToolBarManager manager) {
-//		manager.add(action1);
-//		manager.add(action2);
-//	}
-//
-//	private void makeActions() {
-//		action1 = new Action() {
-//			public void run() {
-//				showMessage("Action 1 executed");
-//			}
-//		};
-//		action1.setText("Action 1");
-//		action1.setToolTipText("Action 1 tooltip");
-//		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-//			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-//		
-//		action2 = new Action() {
-//			public void run() {
-//				showMessage("Action 2 executed");
-//			}
-//		};
-//		action2.setText("Action 2");
-//		action2.setToolTipText("Action 2 tooltip");
-//		action2.setImageDescriptor(workbench.getSharedImages().
-//				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-//		doubleClickAction = new Action() {
-//			public void run() {
-//				IStructuredSelection selection = viewer.getStructuredSelection();
-//				Object obj = selection.getFirstElement();
-//				showMessage("Double-click detected on "+obj.toString());
-//			}
-//		};
-//	}
-//
-//	private void hookDoubleClickAction() {
-//		viewer.addDoubleClickListener(new IDoubleClickListener() {
-//			public void doubleClick(DoubleClickEvent event) {
-//				doubleClickAction.run();
-//			}
-//		});
-//	}
-//	private void showMessage(String message) {
-//		MessageDialog.openInformation(
-//			viewer.getControl().getShell(),
-//			"Content View",
-//			message);
-//	}
-//
-//	@Override
-//	public void setFocus() {
-//		viewer.getControl().setFocus();
-//	}
 }
--- StackOverFlow/src/stackoverflow/handlers/QuickSearchHandler.java
+++ StackOverFlow/src/stackoverflow/handlers/QuickSearchHandler.java
@@ -6,6 +6,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.json.JSONException;
 
@@ -21,39 +22,47 @@ public class QuickSearchHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		String intitle;
-		String title = "";
-		String body = "";
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		UserInputDialog dialog = new UserInputDialog(window.getShell());
-		dialog.create();
-		if (dialog.open() == Window.OK) {
-			intitle = dialog.getSearchText();
-			System.out.println(dialog.getSearchText());
-
-			QuickSearchResult qSearchResult;
-			
-			try {
-				qSearchResult = new QuickSearchResult(intitle);
-				if(qSearchResult.haveResult()) {
-				body = qSearchResult.getBody();
-				title = qSearchResult.getTitle();
-				QuickSearchResultDialog searchResult = new QuickSearchResultDialog(window.getShell(), title, body);
-				searchResult.open();	
-				}
-				else {
-				MessageDialog.openError(window.getShell(), "Error", "not found the result you are searching");
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				
-			}
-
+		
+		try {
+			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView("stackoverflow.ViewAndDialog.ContentView");
+		} catch (PartInitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+//		String intitle;
+//		String title = "";
+//		String body = "";
+//		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+//		UserInputDialog dialog = new UserInputDialog(window.getShell());
+//		dialog.create();
+//		if (dialog.open() == Window.OK) {
+//			intitle = dialog.getSearchText();
+//			System.out.println(dialog.getSearchText());
+//
+//			QuickSearchResult qSearchResult;
+//			
+//			try {
+//				qSearchResult = new QuickSearchResult(intitle);
+//				if(qSearchResult.haveResult()) {
+//				body = qSearchResult.getBody();
+//				title = qSearchResult.getTitle();
+//				QuickSearchResultDialog searchResult = new QuickSearchResultDialog(window.getShell(), title, body);
+//				searchResult.open();	
+//				}
+//				else {
+//				MessageDialog.openError(window.getShell(), "Error", "not found the result you are searching");
+//				}
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			} catch (JSONException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//				
+//			}
+//
+//		} 
+		
 		return null;
 	}
 }
