17e4be11ffa5bf7e6460c6c8318bc42d63b050a8
tewtew023 <abcdtivolitwin@gmail.com>
2020-07-15 15:48:18 +0700

    let button fucntional

--- StackOverFlow/src/stackoverflow/ViewAndDialog/FavoriteView.java
+++ StackOverFlow/src/stackoverflow/ViewAndDialog/FavoriteView.java
@@ -1,10 +1,11 @@
 package stackoverflow.ViewAndDialog;
 
-
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.*;
 import org.json.JSONException;
 
+import stackoverflow.APIConnecter.AllContentObjectOnly;
+import stackoverflow.LocalJsonConnector.ContentWriter;
 import stackoverflow.LocalJsonConnector.FavoriteList;
 
 import org.eclipse.jface.viewers.*;
@@ -19,22 +20,18 @@ import java.io.IOException;
 
 import javax.inject.Inject;
 
-
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
+ * This sample class demonstrates how to plug-in a new workbench view. The view
+ * shows data obtained from the model. The sample creates a dummy model on the
+ * fly, but a real implementation would connect to the model available either in
+ * this or another plug-in (e.g. the workspace). The view is connected to the
+ * model using a content provider.
  * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
+ * The view uses a label provider to define how model objects should be
+ * presented in the view. Each view can present the same model objects using
+ * different labels and icons, if needed. Alternatively, a single label provider
+ * can be shared between views in order to ensure that objects of the same type
+ * are presented in the same way everywhere.
  * <p>
  */
 
@@ -45,9 +42,11 @@ public class FavoriteView extends ViewPart {
 	 */
 	public static final String ID = "stackoverflow.ViewAndDialog.FavoriteView";
 
-	@Inject IWorkbench workbench;
-	
+	@Inject
+	IWorkbench workbench;
+
 	private TableViewer viewer;
+	private String[] title;
 	private Action open;
 	private Action delete;
 	private Action refresh;
@@ -55,17 +54,18 @@ public class FavoriteView extends ViewPart {
 	private Action doubleClickAction;
 	private String[] id;
 	private FavoriteList fav;
-	 
 
 	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
 		@Override
 		public String getColumnText(Object obj, int index) {
 			return getText(obj);
 		}
+
 		@Override
 		public Image getColumnImage(Object obj, int index) {
 			return getImage(obj);
 		}
+
 		@Override
 		public Image getImage(Object obj) {
 			return workbench.getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
@@ -77,7 +77,7 @@ public class FavoriteView extends ViewPart {
 		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
 		viewer.setContentProvider(ArrayContentProvider.getInstance());
 		createTable();
-	viewer.setLabelProvider(new ViewLabelProvider());
+		viewer.setLabelProvider(new ViewLabelProvider());
 
 		// Create the help context id for the viewer's control
 		workbench.getHelpSystem().setHelp(viewer.getControl(), "StackOverFlow.viewer");
@@ -87,11 +87,11 @@ public class FavoriteView extends ViewPart {
 		hookDoubleClickAction();
 		contributeToActionBars();
 	}
-	
+
 	private void createTable() {
 		try {
 			fav = new FavoriteList();
-			String[] title = fav.getTitle();
+		 title = fav.getTitle();
 			id = fav.getID();
 			viewer.setInput(null);
 			viewer.setInput(title);
@@ -100,14 +100,32 @@ public class FavoriteView extends ViewPart {
 			e.printStackTrace();
 		}
 	}
+
 	private void open() {
-		//TODO Call ContentView and send the selection ID in index to Contentview setup
+		// TODO Call ContentView and send the selection ID in index to Contentview setup
 	}
+
 	private void delete() {
-		//TODO add delete function
+
+		int index = viewer.getTable().getSelectionIndex();
+		if (fav.delete(index)) {
+			createTable();
+		}
+		// TODO add delete function
 	}
+
 	private void saveOffline() {
-		//TODO add saveOffline function
+		int index = viewer.getTable().getSelectionIndex();
+		try {
+			new ContentWriter().saveContent(
+					// call AllContentObjectOnly() to create JSON Object
+					new AllContentObjectOnly().getJsonObject(id[index]), id[index], title[index]);
+		} catch (IOException | JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// TODO add saveOffline function
 	}
 
 	private void hookContextMenu() {
@@ -145,10 +163,11 @@ public class FavoriteView extends ViewPart {
 		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 		manager.add(saveOffline);
 	}
-	
+
 	private void fillLocalToolBar(IToolBarManager manager) {
 		manager.add(open);
 		manager.add(delete);
+		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 		manager.add(saveOffline);
 	}
 
@@ -162,7 +181,7 @@ public class FavoriteView extends ViewPart {
 		open.setToolTipText("Open this question on new tab");
 //		open.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
 //			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
+
 		delete = new Action() {
 			public void run() {
 				delete();
@@ -172,7 +191,7 @@ public class FavoriteView extends ViewPart {
 		delete.setToolTipText("delete this record");
 //		delete.setImageDescriptor(workbench.getSharedImages().
 //				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
+
 		saveOffline = new Action() {
 			public void run() {
 				saveOffline();
@@ -182,18 +201,17 @@ public class FavoriteView extends ViewPart {
 		saveOffline.setToolTipText("save this question to Offline list");
 //		saveOffline.setImageDescriptor(workbench.getSharedImages().
 //				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
+
 		refresh = new Action() {
 			public void run() {
-				
+
 				createTable();
-				
-			refresh.setText("Save to offline Storage");
-			refresh.setToolTipText("Refresh this page");
+
+				refresh.setText("Save to offline Storage");
+				refresh.setToolTipText("Refresh this page");
 			}
 		};
-		
-		
+
 		doubleClickAction = new Action() {
 			public void run() {
 				open();
@@ -208,7 +226,7 @@ public class FavoriteView extends ViewPart {
 			}
 		});
 	}
-	
+
 //	private void showMessage(String message) {
 //		MessageDialog.openInformation(
 //			viewer.getControl().getShell(),
