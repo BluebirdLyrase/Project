7fbfebd12bfc394237cd6038273f6b8dd1515f68
tewtew023 <abcdtivolitwin@gmail.com>
2020-11-13 12:52:50 +0700

    Can pull code from active d editor now

--- StackOverFlow/src/stackoverflow/handlers/AutoQHandler.java
+++ StackOverFlow/src/stackoverflow/handlers/AutoQHandler.java
@@ -4,6 +4,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IEditorInput;
 //import javax.xml.namespace.QName;
 //import javax.xml.stream.XMLEventReader;
 //import javax.xml.stream.XMLInputFactory;
@@ -14,44 +15,54 @@ import org.eclipse.jface.dialogs.MessageDialog;
 //import java.util.Collection;
 //import java.util.Map;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IReusableEditor;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.ui.texteditor.StatusTextEditor;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.window.Window;
 
 public class AutoQHandler extends AbstractHandler {
 
-//    public static final String COMPONENT_ID = "StackInTheFlow.TermStatComponent";
-//    private long termCount;
-//    private long docCount;
-//    private Map<String, Stat> termStatMap;
-//    private Collection<Scorer> scorers;
+	private IWorkbenchWindow window;
 
-	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		String currentEditorText;
-		currentEditorText = getCurrentEditorContent();
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"StackOverFlow",
-				currentEditorText);
-		return null;
+
+		window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+
 		
-	}
-	
-	public String getCurrentEditorContent() {
-	    final IEditorPart activeEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-	            .getActiveEditor();
-	    if (activeEditor == null)
-	        return null;
-	    final IDocument doc = (IDocument) activeEditor.getAdapter(IDocument.class);
-	    if (doc == null) return null;
+		IEditorPart	part = ((IWorkbenchPage) PlatformUI.getWorkbench()
+	            .getActiveWorkbenchWindow().getActivePage()).getActiveEditor();
+		
+//		IEditorPart editor = ((IWorkbenchPage) PlatformUI.getWorkbench()
+//	            .getActiveWorkbenchWindow().getActivePage()).getActiveEditor();
+//
+	    IEditorInput input = (IEditorInput) part.getEditorInput();     // accessing code from eclipse editor
+	    System.out.print(input.getAdapter(IEditorInput.class));
+		if (part instanceof ITextEditor) {
+			ITextEditor editor = (ITextEditor) part;
+			IDocumentProvider provider = editor.getDocumentProvider();
+			IDocument document = provider.getDocument(editor.getEditorInput());
 
-	    return doc.get();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-	}
-	
+			MessageDialog.openInformation(window.getShell(), "StackOverFlow", document.get());
 
+			
+
+		}else {System.out.print("nope it's false");}
+		//MessageDialog.openInformation(window.getShell(), "StackOverFlow", "Brah");
+		return null;
+	}
 	
 }
+
+//	public String getCurrentEditorContent() {
+//
+//	}
